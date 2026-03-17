b0d71318dfea8f31f80676a6779f06c9ec7ea964
ARchanaarchu2001 <archanaarchu1797@gmail.com>
2026-03-17 13:47:34 +0400

    Login API completed

--- devtinder_web/src/components/Login.jsx
+++ devtinder_web/src/components/Login.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
 
 const Login = () => {
+
+  const [email, setEmail ] = useState("");
+  const [password, setPassword ] = useState("");
+  
+
+  const handleLogin =async  () => {
+    try {
+      const res = await axios.post("http://localhost:5000/auth/login", { email, password }, { withCredentials: true });
+      console.log("Login Successful ")
+    }
+    catch (error) {
+    console.error("Login Failed ", error )
+    }
+  }
+
+
   return (
+    <div className='flex justify-center m-10'>
+     <div className="card card-border bg-base-300 w-96">
+  <div className="card-body">
+    <h2 className="card-title justify-center py-3">Login</h2>
     <div>
-      Login Page
+      <fieldset className="fieldset">
+  <legend className="fieldset-legend">Email ID</legend>
+  <input type="text" className="input" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Enter your email" />
+ 
+</fieldset>
+<fieldset className="fieldset">
+  <legend className="fieldset-legend">Password</legend>
+  <input type="password" className="input" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Enter your password" />
+ 
+</fieldset>
+    </div>
+   
+    <div className="card-actions justify-center pt-3">
+      <button className="btn btn-primary" onClick={handleLogin}>Login</button>
+    </div>
+  </div>
+</div>
     </div>
   )
 }
