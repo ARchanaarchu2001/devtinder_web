231cd1e56d1b79e06c1ceafe977a605cf3c979de
ARchanaarchu2001 <archanaarchu1797@gmail.com>
2026-03-24 20:27:57 +0400

    build the user card

--- devtinder_web/src/components/Feed.jsx
+++ devtinder_web/src/components/Feed.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect } from 'react'
+import { BASE_URL } from '../utils/constants'
+import { useDispatch, useSelector } from 'react-redux'
+import { addFeed } from '../utils/feedSlice'
+import UserCard from './UserCard'
 
 const Feed = () => {
+
+    const dispatch = useDispatch();
+    //read the feed 
+ const feed = useSelector((store) => store.feed)
+
+    const feedData = async () => {
+      if (feed) return;
+        try { 
+            const res = await axios.get(`${BASE_URL}/user/feed`, { withCredentials: true })
+            dispatch(addFeed(res.data))
+
+
+        }
+        catch (err)
+        {
+            console.error("Error fetching feed data ", err)
+        }
+    }
+
+    useEffect(() => {
+      feedData();
+    },[feed])
+
   return (
-    <div>
-      Feed
+    <div  className='flex justify-center my-10'>
+    {feed &&  (
+  <UserCard user={feed[0]} />
+) 
+  
+}
     </div>
   )
 }
---
+++ devtinder_web/src/components/UserCard.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const UserCard = ({ user }) => {
+  console.log("user in card ", user);
+
+  if (!user) return <p>Loading...</p>;
+
+  const { firstName, lastName, city, photo } = user;
+
+  return (
+    <div className="card bg-base-300 w-96 shadow-sm">
+      <figure>
+        <img src={photo} alt={firstName}   className="w-full h-70 object-cover" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">
+          {firstName + " " + lastName}
+        </h2>
+        <p>{city}</p>
+        <div className="card-actions justify-center ">
+          <button className="btn btn-primary">Ignore</button>
+          <button className="btn btn-secondary">Interested</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserCard
